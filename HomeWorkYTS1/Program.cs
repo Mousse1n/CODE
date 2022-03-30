218835a46b62e9b57421f464f5b63b8cb5038aad
志超 葛 <3529820494@qq.com>
2022-03-30 23:52:19 +0800

    DateTime课堂笔记

--- HomeWorkYTS1/Program.cs
+++ HomeWorkYTS1/Program.cs
@@ -9,9 +9,115 @@ namespace HomeWorkYTS1
 {
     class Program
     {
+        /// <summary>
+        /// 利用ref调用Swap()方法交换两个同学的床位号
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static void Swap(int a, int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
 
+
+        }
+        //public
+        //protected
+        //private 
+        public static double GetAverage(double[] exam)
+        {
+            double max = 0;
+
+            for (int i = 0; i < exam.Length; i++)
+            {
+                //max += exam[i];
+                max = max + exam[i];
+            }
+            double AVG = max / exam.Length;
+
+            return AVG;
+
+        }
+
+        public static bool Login(string username, string pwd)
+        {
+            string user = "yangtongshu";
+            string pwder = "111111";
+            if (username == user && pwd == pwder)
+            {
+                return true;
+                WriteLine("登录成功");
+            }
+            else
+            {
+                return false;
+                WriteLine("用户名密码错误");
+            }
+
+        }
+       /// <summary>
+       /// Gussme
+       /// </summary>
+        public static void GuessMe()
+        {
+            Random Guessnumber = new Random();
+            int GUs = Guessnumber.Next(0, 1000);
+            
+            int GuessNumberturn = 1;//猜测次数
+            bool i = true;
+            while (i)
+            {
+                WriteLine("请输入不超过1000的自然数：");
+                int numberUSer = int.Parse(ReadLine());
+                if (GuessNumberturn == 10)
+                {
+                    WriteLine($"猜了{GuessNumberturn}次，机会用完啦！");
+                    break;
+                }else if (numberUSer < GUs)
+                {
+                    WriteLine("猜小啦");
+                    GuessNumberturn++;
+                    continue;
+                }
+                else if (numberUSer > GUs)
+                {
+                    WriteLine("猜大啦");
+
+                    GuessNumberturn++;
+                    continue;
+                }
+                
+                else if(numberUSer == GUs)
+                {
+                    WriteLine("猜对啦");
+                    break;
+                }
+            }
+            
+            if (GuessNumberturn <= 5)
+            {
+                WriteLine($"答对了你真牛逼，用了{GuessNumberturn}次");
+            }
+            else if (GuessNumberturn <= 8)
+            {
+
+                WriteLine($"答对了不错嘛，用了{GuessNumberturn}次");
+            }
+            else if (GuessNumberturn == 10)
+            {
+                WriteLine($"(～￣(OO)￣)ブ没猜出来啊，用了{GuessNumberturn}次");
+
+            }
+
+        }
         static void Main(string[] args)
         {
+            //double[] Exam = { 99.00, 88.00, 77.00, 11.00, 100.00, 22.00 };
+            //double Avg = Math.Round(GetAverage(Exam), 2);
+            //Console.WriteLine(Avg);
+            GuessMe();
+                
 
             //Random rand = new Random();//调用随机数
             //double radius = rand.Next(10);//固定随机数范围
@@ -63,57 +169,121 @@ namespace HomeWorkYTS1
             // double ShowMin = FindMin(Exam);
             //Console.WriteLine(ShowMax);
             //Console.WriteLine(ShowMin);
-            int[] nums = new int[10];
+            //int[] nums = new int[10];
 
-            Random random = new Random();
+            //Random random = new Random();
 
-            //随机数值
+            ////随机数值
 
-            for (int i = 0; i < 10; i++)
+            //for (int i = 0; i < 10; i++)
 
-            {
+            //{
 
-                nums[i] = random.Next(0, 100);
+            //    nums[i] = random.Next(0, 100);
 
-            }
+            //}
 
-            //冒泡排序 从小到大
+            ////冒泡排序 从小到大
 
-            for (int i = 0; i < 9; i++)
+            //for (int i = 0; i < 9; i++)
 
-            {
+            //{
 
-                for (int j = i + 1; j < 10; j++)
+            //    for (int j = i + 1; j < 10; j++)
 
-                {
+            //    {
 
-                    if (nums[j] < nums[i])
+            //        if (nums[j] < nums[i])
 
-                    {
+            //        {
 
-                        int temp = nums[i];
+            //            int temp = nums[i];
 
-                        nums[i] = nums[j];
+            //            nums[i] = nums[j];
 
-                        nums[j] = temp;
+            //            nums[j] = temp;
 
-                    }
+            //        }
 
-                }
+            //    }
 
-            }
+            //}
 
-            //打印到屏幕
+            ////打印到屏幕
 
-            foreach (int num in nums)
+            //foreach (int num in nums)
 
-            {
+            //{
 
-                Console.Write($"{num},");
+            //    Console.Write($"{num},");
 
-            }
+            //}
+
+            //GetArray();
+            Swap(1, 2);
             ReadLine();
 
+
+        }
+        public static int BinarySeek(int[] numbers, int tarrget, int low, int high)
+        {
+
+
+            int mid = (low + high) / 2;
+            if (low > high)
+                return -1;
+            else
+            {
+                if (numbers[mid] == tarrget)
+                    return mid;
+                else if (numbers[mid] > tarrget)
+                    return BinarySeek(numbers, tarrget, low, mid - 1);
+                else
+                    return BinarySeek(numbers, tarrget, mid + 1, high);
+            }
+
+
+
+
+        }
+        public static int[] GetArray()
+        {
+            Random random = new Random();
+            Random random1 = new Random();
+            Random random2 = new Random();
+            int min = /*random1.Next(1,100)*/1;
+            int gap = 5;
+            int length = /*random2.Next(10,1000)*/10;
+
+            int[] ArrayNumber = new int[length];
+
+            for (int j = 0; j < length; j++)
+            {
+                ArrayNumber[j] = random.Next(min, 100);
+            }//生成最小值可控的随机数
+             //ArrayNumber[]看生成的ArrayNumber是不是最大差值为5如果是，输出如果不是，继续生成。
+            for (int i = 0; i < ArrayNumber.Length; i++)
+            {
+                for (int k = 0; k < ArrayNumber.Length - i - 1; k++)
+                {
+                    if (ArrayNumber[k] > ArrayNumber[k + 1])
+                    {
+                        int temp = ArrayNumber[k];
+                        ArrayNumber[k] = ArrayNumber[k + 1];
+                        ArrayNumber[k + 1] = temp;
+
+                    }
+                }
+            }
+            for (int i = 0; i < ArrayNumber.Length; i++)
+            {
+                Console.Write($"{ArrayNumber[i]} ,");
+            }
+
+
+
+
+            return ArrayNumber;
 
         }
         public static double FindMax(double[] Exam)
@@ -129,7 +299,7 @@ namespace HomeWorkYTS1
             return ExamMax;
 
         }
-        public static double FindMin(double[] Exam) 
+        public static double FindMin(double[] Exam)
         {
             double ExamMin = Exam[0];
             for (int i = 0; i < Exam.Length; i++)
@@ -145,16 +315,16 @@ namespace HomeWorkYTS1
 
             //    //Console.WriteLine(Exam[i]);
             //    Exam[i];
-                
+
             //}
             //for (int j = 1; j < Exam.Length; j++)
             //{
             //    Exam[j];
             //}
-            
-            
+
+
             return ExamMin;
-        
+
         }
         public static double AlertArea(double radius)
         {
@@ -168,4 +338,5 @@ namespace HomeWorkYTS1
             return girth;
         }
     }
+
 }
---
+++ StructTest/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructTest
+{
+    public struct /*class*/ Bed 
+    {
+        //public Bed() 结构不包括显式的无参构造函数，值类型要求其所有成员必须有值，引用类型可以指向null值，因为值类型存储是要在地址中拓展。引用类型是存的地址。
+        //{
+        //不写代码由.net运行时自己控制，怕乱
+        //}
+        //结构是值类型
+        public double Price { get; set; }
+        public  int _number ;
+        public bool HasBooked { get; set; }
+        public Bed(int number) //有值保证以后调用时不会出现null，不然就和引用类型是一样的，逻辑错误。
+        {
+            _number = number;
+            Price = 98.6;
+            HasBooked = true;
+        }
+    
+    }
+    
+    
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string now = "2010/2/22";
+           
+            if (DateTime.TryParse(now, out DateTime result))
+            {
+                result = result.AddYears(10);//传出的是对象，需要变量接受
+                Console.WriteLine(result.Year);
+            }
+            else
+            {
+                Console.WriteLine("wrong format");
+            }
+            
+            //DateTime.Parse();
+            DateTime time;
+            Console.WriteLine(new DateTime(2020,10,29,3,12,20));
+            Console.WriteLine(DateTime.MaxValue );
+             Console.WriteLine(DateTime.Now);
+            /*int*/Int32 age = new Int32();//int 是int32的简写
+            /*long aa = new Int64()*/;
+            //if (age is Int32)
+            //{
+
+            //}
+            //Bed bed = new Bed();
+            long number = 6789;
+            number.CompareTo(age);
+            Console.WriteLine(age);
+           //struct结构
+            int.Parse("");
+            Bed dream /*= new Bed()*/;
+            //dream.HasBooked = false;
+            dream._number = 32;//只有字段可以，方法属性是不行的
+        }
+    }
+}
