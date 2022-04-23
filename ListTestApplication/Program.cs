2d8d16a62dd2e872e8147f5ba98d8164f98841bd
志超 葛 <3529820494@qq.com>
2022-04-23 22:01:30 +0800

    集合笔记1

---
+++ ListTestApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListTestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> score = new List<int> {90,110,100 };
+            score[1] = 120;
+            score.Add(12);
+            score.Add(100);
+            score.BinarySearch(100);
+            score.Insert(0,70);
+            //score.AddRange();
+            score.Remove(100);
+            //score.RemoveAll();
+            score.RemoveAt(1);
+            score.Contains(120);
+            Console.WriteLine(score.IndexOf(120));
+            Console.WriteLine(score.LastIndexOf(100));
+            Console.WriteLine(score[2]);
+            score.Clear();
+            Console.WriteLine(score.Count);
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
