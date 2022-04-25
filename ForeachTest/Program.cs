e353b43e0d2838d6185e3d655da3f1ec17bf73b0
志超 葛 <3529820494@qq.com>
2022-04-25 19:24:36 +0800

    Foreach实现

---
+++ ForeachTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+namespace ForeachTest
+{
+    public class Student 
+    {
+    
+    
+    
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
