2ae820b832f591c36e43fe255d3d207f8dde3a85
志超 葛 <3529820494@qq.com>
2022-05-11 08:11:31 +0800

    添加CodeGame项目

---
+++ CodeGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CodeGame
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
