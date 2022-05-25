5828ae3d55fc6ab1ae952eda97dfc00d5e3055ce
志超 葛 <3529820494@qq.com>
2022-05-25 23:48:47 +0800

    线程笔记

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -3,30 +3,45 @@ using System.Threading;
 
 namespace Multithreading
 {
+
+    public class Person
+    {
+
+        public int Age { get; set; }
+        private string _name;
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            //Thread current = Thread.CurrentThread;
+            //Thread thread = Thread.CurrentThread;
+
             //Console.WriteLine(Thread.GetDomain().FriendlyName);
-            //Console.WriteLine(current.ManagedThreadId);
-            //Console.WriteLine(current.Priority);
-            //Console.WriteLine(current.ThreadState);
-            //Console.WriteLine(current.IsThreadPoolThread);
-            //current.Abort();
-            //current.Suspend();
-            //Thread thread = new Thread(Process);
-            //thread.Start();
-            //Console.WriteLine(thread.ManagedThreadId);
-            //Thread.Sleep(2000);
-            //Console.WriteLine(thread.Priority);
-            //Console.WriteLine(thread.ThreadState);
-            //Console.WriteLine(thread.IsThreadPoolThread);
+
+            //Console.WriteLine(thread.ManagedThreadId);//托管线程ID
+            //Console.WriteLine(thread.Priority);//优先级
+            //Console.WriteLine(thread.ThreadState);//线程状态
+            //Console.WriteLine(thread.IsThreadPoolThread);//是否线程池线程
+            ///*Thread.Sleep(2000);*/                                             //thread.Abort();
+            //Thread thread1 = new Thread(Process);
+            ////thread.Suspend();
+            //Console.WriteLine(thread1.ManagedThreadId);//托管线程ID
+            //Console.WriteLine(thread1.Priority);//优先级
+            //Console.WriteLine(thread1.ThreadState);//线程状态
+            //Console.WriteLine(thread1.IsThreadPoolThread);//是否线程池线程
+            //Thread.Sleep(2000);\
+            for (int i = 0; i < 20; i++)
+            {
+                //Console.WriteLine($"{i}:threadId-{Thread.CurrentThread.ManagedThreadId}");
+                new Thread(() => { Console.WriteLine($"{i}:threadId-{Thread.CurrentThread.ManagedThreadId}"); }).Start();
+            }
         }
         public static void Process()
         {
-            //Console.WriteLine($"ThreadId:{Thread.CurrentThread.ManagedThreadId}");
+
+            Console.WriteLine($".........");
 
         }
     }
